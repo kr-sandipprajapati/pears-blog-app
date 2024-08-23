7392ba37bdeb8f16baa00786685e4d336f1f5e2a
Sandip Prajapati <sandip.prajapati@knackroot.com>
2024-08-23 16:11:23 +0530

    - prettier added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import '../stylesheets/manifest.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "../stylesheets/manifest.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
